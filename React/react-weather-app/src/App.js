d3e82d59a55f2beb713025ba6bf517f41ffd931f
tinyuliu <tinyu1016@gmail.com>
2021-07-19 15:18:12 +0800

    initial project

---
+++ React/react-weather-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherApp from './components/WeatherApp';
+
+function App() {
+  return <WeatherApp />
+}
+
+export default App;
---
+++ React/react-weather-app/src/components/WeatherApp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styled from '@emotion/styled'; 
+
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+const WeatherApp = () => {
+  return (
+    <Container>
+    <WeatherCard>
+      <h1>Weather</h1>
+    </WeatherCard>
+  </Container>
+  );
+};
+
+export default WeatherApp;
