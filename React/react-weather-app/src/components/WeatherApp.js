a8d9ba4d1b7599b3aa93b032197868b7ae59754c
tinyuliu <tinyu1016@gmail.com>
2021-07-19 15:44:49 +0800

    add styled components

--- React/react-weather-app/src/components/WeatherApp.js
+++ React/react-weather-app/src/components/WeatherApp.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import styled from '@emotion/styled'; 
 
+import { ReactComponent as CloudyIcon } from '../images/day-cloudy.svg';
+import { ReactComponent as AirFlowIcon } from '../images/airFlow.svg';
+import { ReactComponent as RainIcon } from '../images/rain.svg';
+import { ReactComponent as RedoIcon } from '../images/refresh.svg';
+
+
 const Container = styled.div`
   background-color: #ededed;
   height: 100%;
@@ -19,13 +25,102 @@ const WeatherCard = styled.div`
   padding: 30px 15px;
 `;
 
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Cloudy = styled(CloudyIcon)`
+  flex-basis: 30%;
+`;
+
+const Redo = styled(RedoIcon)`
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  cursor: pointer;
+`;
+
 const WeatherApp = () => {
   return (
     <Container>
-    <WeatherCard>
-      <h1>Weather</h1>
-    </WeatherCard>
-  </Container>
+      <WeatherCard>
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            23 <Celsius>°C</Celsius>
+          </Temperature>
+          <Cloudy />
+        </CurrentWeather>
+        <AirFlow>
+          <AirFlowIcon />
+          23 m/h
+        </AirFlow>
+        <Rain>
+          <RainIcon />  
+          48%
+        </Rain>
+        <Redo />
+      </WeatherCard>
+    </Container>
   );
 };
 
